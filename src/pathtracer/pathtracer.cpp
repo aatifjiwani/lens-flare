e45b8a96b2386a0c2c7d59849079a845bbf1ebe6
Cassandra Melax <cmelax@berkeley.edu>
2021-05-05 22:20:08 -0700

    added Ritwik's stuff

--- src/pathtracer/pathtracer.cpp
+++ src/pathtracer/pathtracer.cpp
@@ -488,6 +488,142 @@ void PathTracer::draw_ghost(string color, float r1, float r2) {
 	
 }
 
+// begin integration
+Matrix3x3 make_2_matrix(float a, float b, float c, float d) {
+		// | a b 0 |
+		// | c d 0 |
+		// | 0 0 0 |
+		return Matrix3x3(a, b, 0, c, d, 0, 0, 0, 0);
+}
+
+// a 2x2 inversion on a zero-padded 3x3 matrix
+Matrix3x3 invert2x2(Matrix3x3 mat) {
+		float a = mat(0, 0);
+		float b = mat(0, 1);
+		float c = mat(1, 0);
+		float d = mat(1, 1);
+		return 1.0 / (a*d - b*c) * make_2_matrix(d, -b, -c, a);
+}
+
+Matrix3x3 T(float d) {
+		return make_2_matrix(1, d, 0, 1);
+}
+
+Matrix3x3 generate_R(float c, float n1, float n2) {
+		return make_2_matrix(1, 0, c*(n1-n2)/n2, n1/n2);
+}
+
+Matrix3x3 generate_L(float c) {
+		return make_2_matrix(1, 0, 2*c, 1);
+}
+
+float pupil_height = 12.3;
+
+Matrix3x3 Ts[] = {
+		T(7.700),  // 1
+		T(1.850),
+		T(3.520),
+		T(1.850),
+		T(4.180),
+		T(3.000),
+		T(1.850),
+		T(7.270),
+		T(83.91)
+};
+
+float red_refr[] = {1.652, 1.5991, 1, 1.6396, 1, 1, 1.5776, 1.68990, 1};
+float green_refr[] = {1.652, 1.6113, 1, 1.65, 1, 1, 1.5885, 1.6999, 1};
+float blue_refr[] = {1.652, 1.6164, 1, 1.6542, 1, 1, 1.5930, 1.7040, 1};
+float curvatures[] = {1/30.810, 1/-89.350, 1/580.380, 1/-80.630, 1/28.340, 0, 0, 1/32.190, 1/-52.990, 1/81.320};
+
+// assumes color_
+std::vector<Matrix3x3> create_Rs_for_color(float color_refr[]) {
+		std::vector<Matrix3x3> arr;
+		float prev_n = 1.00;
+		// NOTE: hard coded array length for 9 lenses!
+		for (int i = 0; i < 9; i++) {
+				arr.push_back(generate_R(curvatures[i], prev_n, color_refr[i]));
+				prev_n = color_refr[i];
+		}
+		return arr;
+}
+
+std::vector<Matrix3x3> create_Ls() {
+		std::vector<Matrix3x3> arr;
+		for (int i = 0; i < 9; i++) {
+				arr.push_back(generate_L(curvatures[i]));
+		}
+		return arr;
+}
+
+float radii_size(float r1, float r2) {
+		return (r1 - r2) * (r1 - r2);
+}
+
+std::vector<Matrix3x3> Ls = create_Ls();
+
+std::vector<Matrix3x3> R_red = create_Rs_for_color(red_refr);
+std::vector<Matrix3x3> R_blue = create_Rs_for_color(blue_refr);
+std::vector<Matrix3x3> R_green = create_Rs_for_color(green_refr);
+
+Vector2D trace_ray_auto(float r, float theta, int i, int j, std::vector<Matrix3x3> color_R) {
+		// mapping to 3D
+		Vector3D ray = Vector3D(r, theta, 0);
+		
+		int mini = std::min(i, j);
+		int maxj = std::max(i, j);
+		i = mini;
+		j = maxj;
+		
+		Matrix3x3 M = make_2_matrix(1, 0, 0, 1);
+		
+		// forward through j-1
+		for (int k = 0; k < j; k++)
+				M = Ts[k] * color_R[k] * M;
+		
+		// reflect off of Lj
+		M = Ls[j] * M;
+		
+		// cout << "-1. no inv alr" << endl << M << endl;
+		
+		for (int k = i + 1; k > j - 2; k--)
+				M = invert2x2(color_R[k]) * Ts[k] * M;
+		
+		// cout << "0. inv alr" << endl << M << endl;
+		
+		M = Ts[i] * invert2x2(Ls[i]) * Ts[i] * M;
+		
+		// cout << "1." << endl << M << endl;
+		
+		// zero indexed
+		for (int k = i+1; k < 9; k++) {
+				if (k == 5) {
+						Vector3D after_ap = M * ray;
+						if (after_ap.x > 11.6 || after_ap.x < -11.6) {
+								cout << "recasting, got aperature status: " << after_ap.x << endl;
+								float r_a = 11.6;
+								if (r < 0)
+										r_a = -11.5;
+								float r_e = (r_a - M(0, 1) * theta) / M(0, 0);
+								ray = Vector3D(r_e, theta, 0);
+								cout << "ray we're casting: " << ray << endl;
+						}
+						// crossing the aperture
+						M = Ts[k] * M;
+						continue;
+				}
+				M = Ts[k] * color_R[k] * M;
+		}
+		
+		// cout << "2." << endl << M << endl;
+		
+		Vector3D res = M * ray;
+		return Vector2D(res.x, res.y);
+}
+
+
+// end integration
+
 void PathTracer::generate_ghost_buffer() {
 	
 	// initialize size
@@ -506,6 +642,16 @@ void PathTracer::generate_ghost_buffer() {
 	
 	//test
 	draw_ghost("red", -5, 5);
+	
+	for (int i = 0; i < 5; i++) {
+			for (int j = i+1; j < 5; j++) {
+					Vector2D sensor_ray_1 = trace_ray_auto(14.5, angle_to_sun, i, j, R_red);
+					Vector2D sensor_ray_2 = trace_ray_auto(14.5, angle_to_sun, i, j, R_red);
+					draw_ghost("red", sensor_ray_1.x, sensor_ray_1.y);
+//					trace_ray_auto(1, 0.01, i, j, R_green);
+//					trace_ray_auto(1, 0.01, i, j, R_blue);
+			}
+	}
 	
 }
 
